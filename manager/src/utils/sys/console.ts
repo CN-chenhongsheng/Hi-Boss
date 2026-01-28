850d098b01abfdc5588cdc7f3e676881026f07a9
陈鸿昇 <3251612523@qq.com>
2026-01-28 15:29:48 +0800

    feat: 简化系统名称显示   -将系统名称从"宿管系统"简化为"系统"   -更新页面标题、系统配置和控制台欢迎信息
    提交人：陈鸿昇

--- manager/src/utils/sys/console.ts
+++ manager/src/utils/sys/console.ts
@@ -1,6 +1,6 @@
 // ANSI 转义码生成网站  https://patorjk.com/software/taag/#p=display&f=Big&t=ABB%0A
 const asciiArt = `
-\x1b[32m欢迎使用 宿管系统~
+\x1b[32m欢迎使用 系统~
 \x1b[0m
 \x1b[36m哇！你居然在用我的项目～ 祝您使用体验满分💯
 \x1b[0m
